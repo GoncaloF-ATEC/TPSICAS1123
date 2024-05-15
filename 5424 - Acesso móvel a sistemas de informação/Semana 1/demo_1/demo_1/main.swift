64e5c95f2a93cc8668659ec911f03a5b429feef5
Goncalo Feliciano <goncaloaaf@gmail.com>
2024-05-15 18:34:10 +0100

    aula 1 var, e condições

---
+++ 5424 - Acesso móvel a sistemas de informação/Semana 1/demo_1/demo_1/main.swift
@@ -0,0 +1,107 @@
+
+print("Hello, World!")
+
+print("Hello, World!")
+
+var nome = "Gonçalo"
+
+nome = "Novo Nome"
+
+let nomeCt = 10
+// nomeCt = 10
+
+
+var num1 = 10
+var num2 = 10.0
+
+var soma = Double(num1) + num2
+
+var nome2:String = "nome"
+
+var num3:Float = 10.0
+
+print(num2/3)
+print(num3/3)
+
+
+
+var nome_var:Int
+var _nome_var:Int
+var 🐶 = "Cao"
+var π = Float.pi
+
+print(π)
+
+
+
+
+var myName = "Gonçalo"
+var myYear = 2022
+
+
+print("nome: " + myName +  " em "+String(myYear))
+
+print("nome: \(myName) em \(myYear)")
+
+
+if (myYear == 2024) {
+    
+    print("ano curr")
+    
+}else if myYear < 2024{
+    print("no passado")
+}else{
+    print("no Futuro")
+    
+}
+
+
+var dia = 2
+
+switch dia{
+    case 1:
+        print("dom")
+    
+    case 2:
+        print("2f")
+        fallthrough
+    
+    case 3:
+        print("4f")
+    
+    default:
+        print("dia Invalido")
+    
+}
+
+
+/*
+ receba dois num int e some os valores receebidos
+ 
+ */
+
+print("num 1")
+var num22 = readLine()!
+
+print("num 2")
+var num12 = readLine()!
+
+
+
+
+var mum22_i = Int(num22)!
+var mum12_i = Int(num22)!
+
+var soma2 = mum22_i + mum12_i
+
+print(soma2)
+
+
+
+//print(Int(num12))
+
+/*
+ ()
+ {}
+ []
+ */
