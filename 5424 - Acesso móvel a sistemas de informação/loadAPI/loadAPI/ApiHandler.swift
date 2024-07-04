d76ef075cee9a327a0d4d969a5247fd214e58a45
GoncaloF <goncaloaaf@gmail.com>
2024-07-04 20:46:30 +0100

    fix load data win

--- 5424 - Acesso móvel a sistemas de informação/loadAPI/loadAPI/ApiHandler.swift
+++ 5424 - Acesso móvel a sistemas de informação/loadAPI/loadAPI/ApiHandler.swift
@@ -8,6 +8,13 @@
 import Foundation
 
 
+typealias TODOs = [Todo]
+
+enum  myError: Error{
+    
+    case respostaInvalida
+}
+
 class ApiHandler{
     
     var url: URL
@@ -24,23 +31,76 @@ class ApiHandler{
     func loadTodo(id: Int) async throws-> Todo?{
         self.url.append(component: "\(id)" )
         
-        var req = URLRequest(url: self.url)
+        let req = URLRequest(url: self.url)
         
         let (data, resp) = try await URLSession.shared.data(for: req)
         
         guard let resp = resp as? HTTPURLResponse, (200...299).contains(resp.statusCode) else{
             print("erro")
             
-            return nil
-            
+            throw myError.respostaInvalida
         }
         
-        var myData = try JSONDecoder().decode(Todo.self, from: data)
+        let myData = try JSONDecoder().decode(Todo.self, from: data)
         
         self.url.deleteLastPathComponent()
 
         return myData
     }
+    
+    
+    //https://jsonplaceholder.typicode.com/todos
+    func loadAllTodo() async throws-> TODOs?{
+        
+        let req = URLRequest(url: self.url)
+        
+        let (data, resp) = try await URLSession.shared.data(for: req)
+        
+        guard let resp = resp as? HTTPURLResponse, (200...299).contains(resp.statusCode) else{
+            print("erro")
+            
+            throw myError.respostaInvalida
+        }
+        
+        let myData = try JSONDecoder().decode(TODOs.self, from: data)
+        
+
+        return myData
+    }
+    
+    
+    let semaphore = DispatchSemaphore(value: 0)
+    
+    func fetchPostkWith(id:Int, completion: @escaping (Todo) -> Void){
+           
+           url.append(component: "\(id)")
+      //     url.appendPathComponent("\(id)")
+           
+           let dataTask = URLSession.shared.dataTask(with: url) { data, resp, erro in
+               
+               if let erro = erro {
+                   print("Erro: \(erro.localizedDescription)")
+               }
+               
+               guard let resp = resp as? HTTPURLResponse, (200...299).contains(resp.statusCode) else {
+                   
+                   print("resposta invalida")
+                   return
+               }
+               
+               if let dados = data, let dados = try? JSONDecoder().decode(Todo.self, from: dados){
+                   completion(dados)
+                   self.semaphore.signal()
+               }
+           }
+           
+               dataTask.resume()
+                
+                semaphore.wait()
+            
+               url.deleteLastPathComponent()
+       }
+    
 }
 
 /*
--- 5424 - Acesso móvel a sistemas de informação/loadAPI/loadAPI/main.swift
+++ 5424 - Acesso móvel a sistemas de informação/loadAPI/loadAPI/main.swift
@@ -11,10 +11,26 @@ import Foundation
 var loader = ApiHandler(url: "https://jsonplaceholder.typicode.com/todos/")
 
 
-var myTodo = try! await loader.loadTodo(id: 4)
+//var myTodo = try! await loader.loadTodo(id: 4)
 
-print(myTodo!)
+//print(myTodo!)
 
-myTodo = try! await loader.loadTodo(id: 5)
+//myTodo = try! await loader.loadTodo(id: 5)
 
-print(myTodo!)
+//print(myTodo!)
+
+
+//var allTodo = try! await loader.loadAllTodo()
+
+// print(allTodo![9])
+
+
+
+
+loader.fetchPostkWith(id: 8) { todo in
+    print("--------------------")
+    print(todo)
+}
+print("--------------------")
+
+
