dc30cd70a101d4c5f45dfdfe5dd296e4e6b0f384
GoncaloF <goncaloaaf@gmail.com>
2024-07-10 17:23:50 +0100

    meio da aula

---
+++ 5424 - Acesso móvel a sistemas de informação/doird/Kotlin/1_Intro/src/Main.kt
@@ -0,0 +1,98 @@
+fun main() {
+
+    // comnt
+
+    // comnt 1 linha
+
+    /*
+    comnt
+    varias
+    linhas
+
+     */
+
+    // print
+
+
+    println("msg 1")
+    println("msg 2")
+
+    println("----------")
+
+    print("msg 3")
+    print("msg 4")
+
+    println("----------")
+
+    println("msg\n2")
+
+    println("msgs2r\t2")
+    println("msgs\t2")
+
+    //conct
+
+      print("msg 3" + 23)
+
+    //var
+    println("\n----------")
+
+    var nome = "Gonçalo"
+    val nome2: String = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec cursus ipsum non risus rutrum scelerisque. Phasellus volutpat ullamcorper quam id scelerisque. Sed nulla arcu, sagittis et massa quis, scelerisque tincidunt leo. Phasellus ullamcorper purus at iaculis congue. Proin ultricies commodo quam, sit amet imperdiet ligula pellentesque a. Nullam viverra odio risus, at congue leo finibus sit amet. Proin a hendrerit diam. Praesent a quam sed orci tristique dignissim nec ut lorem."
+
+    var idade = 12;
+
+    var b = false
+
+
+    println("nome: $nome\nnome2: $nome\nIdade: $idade\nEstado:$b")
+    println("----------")
+    println("nome: " + nome +"\nnome2: " + nome + "\nIdade: " + idade + "\nEstado:" + b)
+
+    println("----------")
+
+    var nome3 =nome.plus(" Feliciano")
+    println(nome3)
+
+
+    println(nome)
+    nome = "novo nome"
+  println(nome)
+
+
+    // const
+
+    val nomeC = "Gonçalo"
+    println(nomeC)
+    println(nomeC.plus(" Feliciano"))
+
+    //nomeC = "novo"
+
+
+    // if
+
+
+
+    // switch
+
+
+    // while
+
+
+    // range
+
+
+
+    //for
+
+
+
+
+    // funcs
+
+
+
+
+
+
+
+}
