b27e5d8b2b8362da61f585c7c5ff0c8b98ad550e
Goncalo Feliciano <goncaloaaf@gmail.com>
2024-10-03 17:51:31 +0100

    base Stack e Queue

---
+++ 5089 - Programação – Algoritmos/Queue_Stack/Queue.py
@@ -0,0 +1,16 @@
+class Queue:
+
+    def __init__(self):
+        self.data = []
+
+    def isEmpty(self):
+        pass
+
+    def enQueue(self, data):
+        pass
+
+    def deQueue(self):
+        pass
+
+    def next(self):
+        pass
---
+++ 5089 - Programação – Algoritmos/Queue_Stack/Stack.py
@@ -0,0 +1,16 @@
+class Stack:
+
+    def __init__(self):
+        self.data = []
+        
+    def isEmpty(self):
+        pass
+
+    def push(self, item):
+        pass
+
+    def pop(self):
+        pass
+
+    def peek(self):
+        pass
